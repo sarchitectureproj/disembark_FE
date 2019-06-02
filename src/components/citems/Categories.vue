3b008286668fb732f275cb470ab1e6a824fef1d2
Edwin Jimenez <ejjimenezc@unal.edu.co>
2019-06-02 12:14:28 -0500

    Added CRUD for Category!!!!

--- src/components/citems/Categories.vue
+++ src/components/citems/Categories.vue
@@ -1,12 +1,194 @@
 <template>
-    <p>Categories</p>
+    <div>
+
+        <div v-if="previewCategory" class="FloatingPreview">
+            <div class="field" id="category-fields">
+                <label class="label">Name</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.name" :readonly="readMode">
+                </div>
+                <label class="label">Description</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.description" :readonly="readMode">
+                </div>
+                <label class="label">Recoverable</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.recoverable" :readonly="readMode">
+                </div>
+                <label class="label">Delivery</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.delivery" :readonly="readMode">
+                </div>
+            </div>
+
+            <div v-if="createMode">
+                <button @click="addCategory(categoryData)">Create</button>
+            </div>
+            <div v-else>
+                <button v-if="readMode" @click="readMode= false">Edit</button>
+                <button v-else @click="updateCategory(categoryData)">Update</button>
+                <button @click="deleteCategory(categoryData.id)">Delete</button>
+            </div>
+            <button @click="closePreview">Return</button>
+
+        </div>
+
+        <div v-if="!previewCategory">
+            <button @click="newCategory">Add Category</button>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Recoverable</th>
+                        <th>Delivery</th>
+                    </tr> 
+                </thead>
+                <tbody>
+                    <tr v-for="category in allCategories" :key="category.id" @click=fetchcategoryData(category) >
+                        <td>{{ category.id }}</td>
+                        <td>{{ category.name }}</td>
+                        <td>{{ category.description }}</td>
+                        <td>{{ category.recoverable }}</td>
+                        <td>{{ category.delivery }}</td>
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
     import axios from 'axios'
-    const API_URL = 'http://3.95.188.11:5000/graphql';
+    const API_URL = 'http://18.207.188.49:5000/graphql';
 
     export default {
         name: "Categories",
+        data: function() {
+            return {
+                allCategories: [],
+                previewCategory: false,
+                readMode: true,
+                createMode: false,
+                categoryData: { 
+                    id: null, name: null, 
+                    description: null, recoverable: null,
+                    delivery: null
+                }
+            };
+        },
+        props: ['userRol'],
+        methods: {
+            axiosRequest: async function(query){
+                var result = await axios({
+                    method: "POST",
+                    url: API_URL,
+                    data: {
+                        query: query
+                    }
+                });
+                return result
+            },
+            fetchAllCategories: async function(){
+                var query =  `
+                {allCategories{
+                    id,
+                    name,
+                    description,
+                    recoverable,
+                    delivery
+                } }`;
+                try {
+                    var result = await axios({
+                        method: "POST",
+                        url: API_URL,
+                        data: {
+                        query: query
+                    }
+                    });
+                    this.allCategories = result.data.data.allCategories;
+                    
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+            closePreview: function (){
+                this.previewCategory = false;
+                this.readMode= true;
+                this.createMode = false;
+            },
+            fetchcategoryData: function(category){
+                this.previewCategory = true;
+                this.categoryData = category;
+            },
+            newCategory(){
+                this.categoryData = { 
+                    id: null, name: null, 
+                    description: null, recoverable: null,
+                    delivery: null
+                }
+                this.previewCategory = true;
+                this.readMode= false;
+                this.createMode = true;
+            },
+            addCategory(category){
+                this.previewCategory = false;
+                this.readMode= true;
+                var query =  'mutation{'+
+                    'createCategory('+
+                    'category:{'+
+                        'name: "'+category.name+'",'+
+                        'description: "'+category.description+'",'+
+                        'recoverable: '+category.recoverable+','+
+                        'delivery: ' +category.delivery+','+
+                    '}){id}}';
+                try {
+                    this.axiosRequest(query);
+                } catch (error) {
+                    console.log(error);
+                } finally{
+                    this.fetchAllCategories();
+                    this.previewCategory = false;
+                }
+            },
+            updateCategory: function(category){
+                this.readMode= true;
+
+                var query =  'mutation{'+
+                    'updateCategory(id: '+category.id+','+
+                    'category:{'+
+                        'name: "'+category.name+'",'+
+                        'description: "'+category.description+'",'+
+                        'recoverable: '+category.recoverable+','+
+                        'delivery: ' +category.delivery+','+
+                    '}){name}}';
+                    
+                try {
+                    this.axiosRequest(query);
+                } catch (error) {
+                    console.log(error);
+                } finally{
+                    this.fetchAllCategories();
+                    this.previewCategory = false;
+                }
+            },
+            deleteCategory: function(id){
+                this.readMode= true;
+                var query =  'mutation{deleteCategory(id: '+id+')}';
+                try {
+                    this.axiosRequest(query);
+                } catch (error) {
+                    console.log(error)
+                } finally{
+                    this.fetchAllCategories();
+                    this.previewCategory = false;
+                }
+
+            }
+        },
+        mounted() {
+            this.fetchAllCategories();
+        }
     };
 </script>
--- src/components/citems/Deliveries.vue
+++ src/components/citems/Deliveries.vue
@@ -1,12 +1,227 @@
 <template>
-    <p>Deliveries</p>
+    <div>
+
+        <div v-if="previewItem" class="FloatingPreview">
+            <div class="field" id="category-fields">
+                <label class="label">Item Name</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.name" :readonly="readMode">
+                </div>
+                <label class="label">Confiscation Date</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.confiscation_date" :readonly="readMode">
+                </div>
+                <label class="label">Confiscation Time</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.confiscation_time" :readonly="readMode">
+                </div>
+                <label class="label">Quantity</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.quantity" :readonly="readMode">
+                </div>
+                <label class="label">Category</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.category" :readonly="readMode">
+                </div>
+                <label class="label">Passenger</label>
+                <div class="control">
+                    <input  class="input" type="text" v-model="categoryData.passenger" :readonly="readMode">
+                </div>
+            </div>
+
+            <div v-if="createMode">
+                <button @click="addItem(categoryData)">Create</button>
+            </div>
+            <div v-else>
+                <button v-if="readMode" @click="readMode= false">Edit</button>
+                <button v-else @click="updateItem(categoryData)">Update</button>
+                <button @click="deleteItem(categoryData.id)">Delete</button>
+            </div>
+            <button @click="closePreview">Return</button>
+
+        </div>
+
+        <div v-if="!previewItem">
+            <button @click="newItem">Add Item</button>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Confiscation Date</th>
+                        <th>Confiscation Time</th>
+                        <th>Quantity</th>
+                        <th>Category</th>
+                        <th v-if="userRol==1">Passenger</th>
+                    </tr> 
+                </thead>
+                <tbody>
+                    <tr v-for="category in allItems" :key="category.id" @click=fetchItemData(category) >
+                        <td>{{ category.id }}</td>
+                        <td>{{ category.name }}</td>
+                        <td>{{ category.confiscation_date }}</td>
+                        <td>{{ category.confiscation_time }}</td>
+                        <td>{{ category.quantity }}</td>
+                        <td>{{ category.category }}</td>
+                        <td v-if="userRol==1">{{ category.passenger }}</td>
+                    </tr> 
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
     import axios from 'axios'
-    const API_URL = 'http://3.95.188.11:5000/graphql';
+    const API_URL = 'http://18.207.188.49:5000/graphql';
 
     export default {
-        name: "Deliveries",
+        name: "ItemList",
+        data: function() {
+            return {
+                allItems: [],
+                previewItem: false,
+                readMode: true,
+                createMode: false,
+                categoryData: { 
+                    id: null, name: null, 
+                    confiscation_date: null, confiscation_time: null,
+                    quantity: null, category: null, 
+                    passenger: null
+                }
+            };
+        },
+        props: ['userRol'],
+        methods: {
+            axiosRequest: async function(query){
+                var result = await axios({
+                    method: "POST",
+                    url: API_URL,
+                    data: {
+                        query: query
+                    }
+                });
+                return result
+            },
+            fetchAllItems: async function(){
+                var query =  `
+                {allItems{
+                    id,
+                    name,
+                    confiscation_date,
+                    confiscation_time,
+                    quantity,
+                    category,
+                    passenger
+                } }`;
+                var queryPas =  `
+                {allItemsbyPass(passenger: "1997"){
+                    id,
+                    name,
+                    confiscation_date,
+                    confiscation_time,
+                    quantity,
+                    category
+                }}`;
+                if(this.userRol==2){query = queryPas}
+                try {
+                    var result = await axios({
+                        method: "POST",
+                        url: API_URL,
+                        data: {
+                        query: query
+                    }
+                    });
+                    if(this.userRol==2){
+                        this.allItems = result.data.data.allItemsbyPass;
+                    }else{
+                        this.allItems = result.data.data.allItems;
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+            closePreview: function (){
+                this.previewItem = false;
+                this.readMode= true;
+                this.createMode = false;
+            },
+            fetchItemData: function(category){
+                this.previewItem = true;
+                this.categoryData = category;
+            },
+            newItem(){
+                this.categoryData = { 
+                    id: null, name: null, 
+                    confiscation_date: null, confiscation_time: null,
+                    quantity: null, category: null, 
+                    passenger: null
+                }
+                this.previewItem = true;
+                this.readMode= false;
+                this.createMode = true;
+            },
+            addItem(category){
+                this.previewItem = false;
+                this.readMode= true;
+                var query =  'mutation{'+
+                    'createItem('+
+                    'category:{'+
+                        'name: "'+category.name+'",'+
+                        'confiscation_date: "'+category.confiscation_date+'",'+
+                        'confiscation_time: "'+category.confiscation_time+'",'+
+                        'quantity: ' +category.quantity+','+
+                        'category: '+category.category+','+
+                        'passenger: "'+category.passenger+'"'+
+                    '}){id}}';
+                try {
+                    this.axiosRequest(query);
+                } catch (error) {
+                    console.log(error);
+                } finally{
+                    this.fetchAllItems();
+                    this.previewItem = false;
+                }
+            },
+            updateItem: function(category){
+                this.readMode= true;
+
+                var query =  'mutation{'+
+                    'updateItem(id: '+category.id+','+
+                    'category:{'+
+                        'name: "'+category.name+'",'+
+                        'confiscation_date: "'+category.confiscation_date+'",'+
+                        'confiscation_time: "'+category.confiscation_time+'",'+
+                        'quantity: ' +category.quantity+','+
+                        'category: '+category.category+','+
+                        'passenger: "'+category.passenger+'"'+
+                    '}){name}}';
+                    
+                try {
+                    this.axiosRequest(query);
+                } catch (error) {
+                    console.log(error);
+                } finally{
+                    this.fetchAllItems();
+                    this.previewItem = false;
+                }
+            },
+            deleteItem: function(id){
+                this.readMode= true;
+                var query =  'mutation{deleteItem(id: '+id+')}';
+                try {
+                    this.axiosRequest(query);
+                } catch (error) {
+                    console.log(error)
+                } finally{
+                    this.fetchAllItems();
+                    this.previewItem = false;
+                }
+
+            }
+        },
+        mounted() {
+            this.fetchAllItems();
+        }
     };
 </script>
