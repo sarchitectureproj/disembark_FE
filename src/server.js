340334e11250706885478a21ea11209d71b88c7c
nicolasprr <niparrara@unal.edu.co>
2019-06-03 18:14:53 -0500

    url environment

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-const GRAPHQL_URL = 'http://192.168.99.106:5000/graphql';
+const GRAPHQL_URL = process.env.GRAPHQL_URL;
 //const GRAPHQL_URL = 'http://dnode2.centralus.cloudapp.azure.com:5000/graphql';
-
 export default GRAPHQL_URL; 
