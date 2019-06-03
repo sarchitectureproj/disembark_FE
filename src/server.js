f48f2fd9a53267e743ef8785d43e07025a896ca7
Juan Sebastian Alcina <jsalcinar@unal.edu.co>
2019-06-03 16:38:16 -0500

    Changes in passenger Front

--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 //const GRAPHQL_URL = 'http://192.168.99.106:5000/graphql';
 const GRAPHQL_URL = 'http://dnode2.centralus.cloudapp.azure.com:5000/graphql';
-
-=======
-//const GRAPHQL_URL = process.env.GRAPHQL_URL;
-const GRAPHQL_URL = 'http://dnode2.centralus.cloudapp.azure.com:5000/graphql';
->>>>>>> d7363618db4d94198280a04a5ce1fbb4d59ad6d6
 export default GRAPHQL_URL; 
