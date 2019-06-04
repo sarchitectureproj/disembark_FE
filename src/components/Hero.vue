2ab40db74e35fa54537f8dceaa754cc93bc8caae
Kevin Castro <keacastroga@unal.edu.co>
2019-06-03 23:19:10 -0500

    Change home

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,15 +7,15 @@
           </div>
           <div class="column notification custom">
             <h1 class="is-size-1" >Disembark system</h1>
-            <h2>This is a description system</h2>
+            <h2>Crew Interface</h2>
             <h1 class="is-size-2">Functions</h1>
             <div class="content">
               <ol class="is-lower-alpha">
-                <li>Deck:Shedules and meetings points</li>
-                <li>Passangers: Create a new passager</li>
-                <li>Confiscated items: Weapons, chemicals</li>
-                <li>Luggages:Tags administration</li>
-                <li>Crews: crew system</li>
+                <li>Deck: Shedules and meetings points</li>
+                <li>Passangers: Manage passangers</li>
+                <li>Confiscated Items: Management of weapons, chemicals and various forbidden items</li>
+                <li>Luggages: Handle luggage administration by use of color tags</li>
+                <li>Crews: Login system for the crew</li>
               </ol>
             </div>
           </div>
