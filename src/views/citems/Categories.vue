2f9b31970e2fe2b3fd7f85fd705cd215bb63b068
Edwin <ejjimenezc@unal.edu.co>
2019-06-06 20:12:25 +0000

    New Table

--- src/views/citems/Categories.vue
+++ src/views/citems/Categories.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
       <a class="button is-info" href="items">Items</a>
-      <a class="button is-info" href="categories">Categories</a>
+      <a class="button is-danger" href="categories">Categories</a>
       <a class="button is-info" href="deliveries">Deliveries</a>
     </div>
 </template>
-
 <script>
 
     export default {
--- src/views/citems/Deliveries.vue
+++ src/views/citems/Deliveries.vue
@@ -2,7 +2,7 @@
     <div>
       <a class="button is-info" href="items">Items</a>
       <a class="button is-info" href="categories">Categories</a>
-      <a class="button is-info" href="deliveries">Deliveries</a>
+      <a class="button is-danger" href="deliveries">Deliveries</a>
     </div>
 </template>
 
