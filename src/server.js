47d23a4f95479c55f37dc7f0b4dc1f2bb197a533
Kevin Castro <keacastroga@unal.edu.co>
2019-06-17 22:28:50 -0500

    fix

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 //const GRAPHQL_URL = 'http://192.168.99.109:5000/graphql';
-const GRAPHQL_URL = 'http://dnode2.centralus.cloudapp.azure.com:5000/graphql';
+const GRAPHQL_URL = 'http://dnode2.centralus.cloudapp.azure.com/graphql';
 export default GRAPHQL_URL; 
